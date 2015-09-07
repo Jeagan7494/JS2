82944634007a5792f5720234e26d199d9540182e
Student <student@RU-104.local>
2015-09-07 13:29:23 -0600

    after lunch Monday

---
+++ JS2.js
@@ -0,0 +1,9 @@
+var word = prompt("Enter a word below:");
+
+	console.log("You entered: " + word);
+	console.log("There are " + word.length + " characters in the word.");
+	console.log("The third character is " + word.charAt(3));
+	console.log("Lowercase: " + word.toLowerCase());
+	console.log("Uppercase: " + word.toLowerCase())
+	console.log("Great word choice of " + word+ "!")
+	console.log("Subword: " + word.substring(1,4));
